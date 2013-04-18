af6d15ffa1d41977dedf7eeb56f2007663e65198
Richard Louapre <richard.louapre@gmail.com>
2013-04-18 08:07:24 -0400

    Add Url in App.config
    - Add try catch

--- test-nancy-rest/Program.cs
+++ test-nancy-rest/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Nancy;
 using Nancy.Hosting.Self;
+using System.Configuration;
 
 namespace Test.Nancy.Rest
 {
@@ -12,12 +13,25 @@ namespace Test.Nancy.Rest
         static void Main(string[] args)
         {
             string url = "http://localhost:999";
-            var nancyHost = new NancyHost(new Uri(url));
-            nancyHost.Start();
-            Console.WriteLine(String.Format("Server listening on url: {0}", url));
-            Console.WriteLine("Hit <ENTER> to end the process.");
-            Console.ReadLine();
-            nancyHost.Stop();
+            if (ConfigurationManager.AppSettings["Url"] != null)
+            {
+                url = ConfigurationManager.AppSettings["Url"];
+            }
+            try
+            {
+                Console.WriteLine(String.Format("Server listening on url: {0}", url));
+                var nancyHost = new NancyHost(new Uri(url));
+                nancyHost.Start();
+                Console.WriteLine("Hit <ENTER> to end the process.");
+                Console.ReadLine();
+                nancyHost.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadLine();
+            }
         }
     }
 }
